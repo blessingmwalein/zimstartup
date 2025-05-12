ffc68a5d54d50351bd15e5a218a80583c34648ea
Blessing Mwale <blessingmwalein@outlook.com>
2025-05-12 11:23:45 +0200

    Added login home

--- app/login/page.js
+++ app/login/page.js
@@ -3,6 +3,10 @@ import LoginForm from "./Form";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import Image from "next/image";
+import HomeSlider from "@/components/HomeSlider";
+import Link from "next/link";
+
 
 const Login = async () => {
   const session = await getServerSession(authOptions());
@@ -12,49 +16,54 @@ const Login = async () => {
   }
 
   return (
-    <main className=" flex flex-col  ">
-      <div className=" block md:flex ">
-        <div className="flex-1 flex flex-col relative md:h-[96vh] md:mt-[-85px] z-[1000]">
-          <div className="absolute top-3 left-3 w-[150px] md:w-[220px] z-[100] hidden md:block">
+
+    <main>
+      <div className="mx-auto">
+        <nav className="flex justify-between items-center px-8 py-4 bg-[#052941] text-white shadow-md">
+          <Link className="inline-block" href="/">
             <Image
-              src="/images/logo1.webp"
-              width={300}
-              height={110}
-              style={{ width: "100%", height: "auto" }}
-              alt="Zimstartup"
+              className="hidden dark:block"
+              src={"/images/logo1.webp"}
+              alt="Logo"
+              width={200}
+              height={50}
+            />
+            <Image
+              className="dark:hidden"
+              src={"/images/logo1.webp"}
+              alt="Logo"
+              width={200}
+              height={80}
+            />
+          </Link>
+          <div className="space-x-6 text-sm font-medium text-white-600">
+            <Link href="/" className="hover:text-lime-600">Home</Link>
+            <Link href="/helpdesk" className="hover:text-lime-600">HelpDesk</Link>
+            <Link href="/about" className="hover:text-lime-600">About</Link>
+          </div>
+        </nav>
+        <div className="flex h-[calc(100vh-64px)]">
+          <div className="w-2/3 h-full relative overflow-hidden">
+            <Image
+              src="/images/wallpaper.webp"
+              alt="Banner"
+              fill
+              className="object-cover"
+              priority
             />
           </div>
 
-          <HomeSlider />
-
-          {/* <div className="bg-[#D5D58D] flex-1  py-[60px] px-[40px] flex flex-col justify-center ">
-          <div className="max-w-[1300px] w-full mx-auto flex justify-between items-center">
-            <h2 className="text-white">COM1 LOGO</h2>
-            <h2 className="text-white">COM2 LOGO</h2>
-            <h2 className="text-white">COM3 LOGO</h2>
-            <h2 className="text-white">COM4 LOGO</h2>
+          <div className="w-1/3 h-full flex items-center justify-center bg-gray-50">
+         
+              <LoginForm />
+           
           </div>
-        </div> */}
-        </div>
-
-        <div className=" mx-auto p-[10px] pt-[40px] sm:p-[30px] lg:pt-[80px]">
-          <LoginForm />
         </div>
       </div>
-
-      {/* <div className="max-w-[1300px] w-full  mb-1 px-2 md:px-9 ">
-      <h2 className=" mt-10 md:pt-1 ">Featured Companies</h2>
-    </div> */}
-
-      {/* <div className="bg-[#D5D58D]  py-[70px] px-[40px] ">
-      <div className="max-w-[1300px] w-full mx-auto flex justify-between items-center">
-        <h1 className="text-white">COMP1 LOGO</h1>
-        <h1 className="text-white">COMP2 LOGO</h1>
-        <h1 className="text-white">COMP3 LOGO</h1>
-        <h1 className="text-white">COMP4 LOGO</h1>
-      </div>
-    </div> */}
     </main>
+
+
+
   );
 };
 
